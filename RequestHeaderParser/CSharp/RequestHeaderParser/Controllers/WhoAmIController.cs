e261ddcefe7f553159072b2c6595f29bab0425f4
Daniel Rubio <drubio432@gmail.com>
2021-02-11 22:58:16 -0500

    FreeCodeCamp Project Request Header Parser c#

---
+++ RequestHeaderParser/CSharp/RequestHeaderParser/Controllers/WhoAmIController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
+using RequestHeaderParser.Entities;
+
+namespace RequestHeaderParser.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WhoAmIController : ControllerBase
+    {
+        private readonly ILogger<WhoAmIController> _logger;
+
+        public WhoAmIController(ILogger<WhoAmIController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<RequestHeader> Get()
+        {
+            var requestHeaders = new RequestHeader
+            {
+                Ipaddress = Dns.GetHostAddresses(Dns.GetHostName())[4].ToString(),
+                Language = Request.Headers[HeaderNames.AcceptLanguage].ToString(),
+                Software = Request.Headers[HeaderNames.UserAgent].ToString()
+            };
+            return Ok(requestHeaders);
+        }
+    }
+}
---
+++ RequestHeaderParser/CSharp/RequestHeaderParser/Entities/RequestHeader.cs
@@ -0,0 +1,9 @@
+namespace RequestHeaderParser.Entities
+{
+    public class RequestHeader
+    {
+        public string Ipaddress { get; set; }
+        public string Language { get; set; }
+        public string Software { get; set; }
+    }
+}
