93763921202389715feccdc9fe13fd6b7cec696e
Daniel Rubio <drubio432@gmail.com>
2021-03-21 23:02:23 -0400

    add folder structure and index html

--- Catdex/Rust/catdex/src/main.rs
+++ Catdex/Rust/catdex/src/main.rs
@@ -1,14 +1,17 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_files::NamedFile;
+use actix_web::{web, App, HttpServer, Result};
 
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world")
+const ADDRESS: &str = "127.0.0.1:8080";
+
+async fn index() -> Result<NamedFile> {
+    Ok(NamedFile::open("./static/index.html")?)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Listening on port 8080");
-    HttpServer::new(|| App::new().route("/hello", web::get().to(hello)))
-        .bind("127.0.0.1:8080")?
+    HttpServer::new(|| App::new().route("/", web::get().to(index)))
+        .bind(ADDRESS)?
         .run()
         .await
 }
