20081533d2d239f96baf74d34c55964104f0f6e5
Daniel Rubio <drubio432@gmail.com>
2021-03-28 19:00:08 -0400

    add packages for mongo and env

--- Authentication/NodeJS/src/index.js
+++ Authentication/NodeJS/src/index.js
@@ -1,3 +1,32 @@
-import './db.js';
+import './env.js';
+import { fastify } from 'fastify';
+import fastifyStatic from 'fastify-static';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-console.log('Hola Mundo');
+console.log(process.env.MONGO_URL);
+
+// ESM specific to get access to dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = fastify();
+app.register(fastifyStatic, {
+  root: path.join(__dirname, 'public'),
+});
+
+// app.get('/', async (request, reply) => {
+//   return { hello: 'world' };
+// });
+
+const startApp = async () => {
+  try {
+    await app.listen(3000);
+    console.log('Server Listening at port: 3000');
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+startApp();
