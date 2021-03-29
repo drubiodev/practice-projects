232be3b320d4f3cf52048610aae81e4bfd7ca25a
Daniel Rubio <drubio432@gmail.com>
2021-03-28 22:35:58 -0400

    moved server code to own file

--- Authentication/NodeJS/src/index.js
+++ Authentication/NodeJS/src/index.js
@@ -1,28 +1,5 @@
 import './env.js';
-import { logInfo, logError } from './log/index.js';
-import { fastify } from 'fastify';
-import fastifyStatic from 'fastify-static';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { startApp } from './server.js';
 import { connectDb } from './db.js';
-
-// ESM specific to get access to dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const app = fastify();
-app.register(fastifyStatic, {
-  root: path.join(__dirname, 'public'),
-});
-
-const startApp = async () => {
-  try {
-    await app.listen(process.env.PORT);
-    logInfo(`Server Listening at port: ${process.env.PORT}`);
-  } catch (error) {
-    logError(error);
-    process.exit(1);
-  }
-};
 
 connectDb().then(() => startApp());
---
+++ Authentication/NodeJS/src/server.js
@@ -0,0 +1,25 @@
+import { logInfo, logError } from './log/index.js';
+import { fastify } from 'fastify';
+import fastifyStatic from 'fastify-static';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// ESM specific to get access to dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = fastify();
+
+app.register(fastifyStatic, {
+  root: path.join(__dirname, 'public'),
+});
+
+export const startApp = async () => {
+  try {
+    await app.listen(process.env.PORT);
+    logInfo(`Server Listening at port: ${process.env.PORT}`);
+  } catch (error) {
+    logError(error);
+    process.exit(1);
+  }
+};
