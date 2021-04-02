2ead96a6e1bfda0737f2fb1386ff05f053807c5e
Daniel Rubio <drubio432@gmail.com>
2021-04-01 22:26:09 -0400

    added error if fail to register

--- Authentication/NodeJS/src/accounts/register.js
+++ Authentication/NodeJS/src/accounts/register.js
@@ -2,20 +2,24 @@ import pkg from 'bcryptjs';
 const { genSalt, hash } = pkg;
 
 export const registerUser = async (email, password) => {
-  // wont load till called
-  const { user } = await import('../user/user.js');
-  // generate salt
-  const salt = await genSalt(10);
-  // hash with salt
-  const hashPassword = await hash(password, salt);
-  // store in database
-  const result = await user.insertOne({
-    email: {
-      address: email,
-      verified: false,
-    },
-    password: hashPassword,
-  });
-  // return user
-  return result.insertedId;
+  try {
+    // wont load till called
+    const { user } = await import('../user/user.js');
+    // generate salt
+    const salt = await genSalt(10);
+    // hash with salt
+    const hashPassword = await hash(password, salt);
+    // store in database
+    const result = await user.insertOne({
+      email: {
+        address: email,
+        verified: false,
+      },
+      password: hashPassword,
+    });
+    // return user
+    return result.insertedId;
+  } catch (error) {
+    throw `Error Registering User: ${error}`;
+  }
 };
